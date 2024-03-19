9792ee1a65754dd85d24ccb9b2936f65f65fe174
taranchella <taranchella@icloud.com>
2024-03-19 19:02:41 +0300

    one

--- src/test/java/ru/netology/service/RebookDateTest.java
+++ src/test/java/ru/netology/service/RebookDateTest.java
@@ -11,7 +11,6 @@ import java.time.Duration;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selenide.sleep;
 
 class RebookDateTest {
 
@@ -29,36 +28,16 @@ class RebookDateTest {
         var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
         $("[placeholder='Город']").setValue(validUser.getCity());
-        //sleep(2000);
         $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
-        //sleep(2000);
         $("[placeholder='Дата встречи']").setValue(firstMeetingDate);
-        //sleep(2000);
         $("[name='name']").setValue(validUser.getName());
-        //sleep(2000);
         $("[name='phone']").setValue(validUser.getPhone());
-        //sleep(2000);
         $(".checkbox__box").click();
-        //sleep(2000);
         $$("button").find(Condition.exactText("Запланировать")).click();
-        //sleep(2000);
-
-        if ($(withText("Доставка в выбранный город недоступна")).isDisplayed()) {
-            int i = validUser.getCity().length() - 2;
-            while (i > 0) {
-                $("[data-test-id=city] .input__control").sendKeys(Keys.BACK_SPACE);
-                i--;
-            }
-            $(".menu-item__control").click();
-            $(".button__text").click();
-        }
         $("[data-test-id=success-notification]>.notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstMeetingDate));
         $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
-        //sleep(2000);
         $("[placeholder='Дата встречи']").setValue(secondMeetingDate);
-        //sleep(2000);
         $$("button").find(Condition.exactText("Запланировать")).click();
-        //sleep(2000);
         $(withText("Необходимо подтверждение")).shouldBe(Condition.visible, Duration.ofSeconds(15));
         $("div.notification__content > button").click();
         $("[data-test-id=success-notification]>.notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondMeetingDate));
