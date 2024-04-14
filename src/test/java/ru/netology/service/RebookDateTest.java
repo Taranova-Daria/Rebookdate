a9df1834286d953d6dcd302e9b473a04acc7a76d
taranchella <taranchella@icloud.com>
2024-04-14 17:44:41 +0300

    one

--- src/test/java/ru/netology/service/RebookDateTest.java
+++ src/test/java/ru/netology/service/RebookDateTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import com.codeborne.selenide.Condition;
 import org.openqa.selenium.Keys;
 
@@ -17,6 +17,16 @@ class RebookDateTest {
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
+    }
+
+    @BeforeAll
+    static void setupAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
     }
 
     @Test
